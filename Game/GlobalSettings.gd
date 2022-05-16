c7653445d00990ecb57754479354f8d215ae57df
William Reima <william.reima@gmail.com>
2022-05-16 17:55:35 +0300

    Option menu items actually saving now

--- Game/GlobalSettings.gd
+++ Game/GlobalSettings.gd
@@ -2,7 +2,11 @@ extends Node
 
 func toggle_fullscreen(value):
 	OS.window_fullscreen = value
+	Save.game_data.fullscreen_on = value
+	Save.save_data()
 
 
 func toggle_vsync(value):
 	OS.vsync_enabled = value
+	Save.game_data.vsync_on = value
+	Save.save_data()
--- Game/Settings.gd
+++ Game/Settings.gd
@@ -7,6 +7,13 @@ onready var vsync_button = $CenterContainer/VBoxContainer/Label/VSyncButton
 #	display_options.select(true if Save.game_data.fullscreen_on else false)
 #	GlobalSettings.toggle_fullscreen(Save.game_data.fullscreen_on)
 
+func _ready():
+	display_options.select(1 if Save.game_data.fullscreen_on else 0)
+	GlobalSettings.toggle_fullscreen(Save.game_data.fullscreen_on)
+	vsync_button.pressed = Save.game_data.vsync_on
+
+
+
 func _on_FullscreenBtn_item_selected(index):
 	GlobalSettings.toggle_fullscreen(true if index == 1 else false)
 
