ffbe95914fa5df155ec5e0383439c76a8b6277ac
William Reima <william.reima@gmail.com>
2022-09-12 23:57:14 +0300

    started physics and a test level qith player and world collisions

--- Src/Actors/Actor.gd
+++ Src/Actors/Actor.gd
@@ -1,8 +1,11 @@
 extends KinematicBody2D
 class_name Actor
 
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 3000.0
 var velocity: = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	
-	move_and_slide(velocity)
+	velocity.y += gravity * delta
+	#velocity.y = max(velocity.y, speed.y)
+	velocity = move_and_slide(velocity)
