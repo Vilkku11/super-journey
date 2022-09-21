f373dd690d5a0b270a2c9d6b3191c98e9a7b5088
William Reima <william.reima@gmail.com>
2022-09-21 23:40:16 +0300

    new game yes button changes scene to testScene

--- Game/BackFromNewGame.gd
+++ Game/BackFromNewGame.gd
@@ -4,3 +4,10 @@ extends CenterContainer
 
 func _on_BackButton_pressed():
 	get_tree().change_scene("res://Control.tscn")
+
+
+
+	
+
+func _on_YesButton_pressed():
+	get_tree().change_scene("res://Src/Levels/LevelTemplate.tscn")
